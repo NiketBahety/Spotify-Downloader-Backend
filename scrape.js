335fe80a944fa6513a62fcaf1c0cbfddc391cb34
NiketBahety <niketbahety@gmail.com>
2023-07-29 16:38:44 +0530

    Updated args and executable path for puppeteer launch

--- scrape.js
+++ scrape.js
@@ -1,7 +1,20 @@
 const puppeteer = require("puppeteer");
+require("dotenv").config();
 
 const scrape = async (url) => {
-  const browser = await puppeteer.launch({ headless: "new" });
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+    headless: "new",
+  });
   const page = await browser.newPage();
 
   await page.goto(url);
